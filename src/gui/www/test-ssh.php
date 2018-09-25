358e83a890bdac3ee18b948c4dd8bf57d8e7aabc
datlv <datle2011@gmail.com>
2018-09-25 13:54:42 +0700

    fix

--- src/gui/www/test-ssh.php
+++ src/gui/www/test-ssh.php
@@ -1,8 +1,7 @@
 <?php
 ini_set('display_errors',1);error_reporting(E_ALL);
-$htmlData = file_get_contents('http://insight.localhost/report.html');
-$htmlData = preg_replace('#\<div id="round 0"\>(.+?)provided\<\/pre\>#s', "", $htmlData);
-$htmlFile = fopen("/opt/gopath/src/github.com/hyperledger/caliper/src/gui/www/report1.html", "w");
-fwrite($htmlFile, $htmlData);
-fclose($htmlFile);
+$connect = ssh2_connect('127.0.0.1', 22);
+if (ssh2_auth_password($connect, 'datlv', 'tango12@')) {
+    echo "<br>connect";
+}
 ?>
